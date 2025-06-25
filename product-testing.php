75460754970ccda99880461fafb86f87a80e1529
baseone-spec <buyerbaseoneindustrial@gmail.com>
2025-06-25 11:02:28 +0800

    initial commit@

--- product-testing.php
+++ product-testing.php
@@ -10,9 +10,28 @@
 
 <body>
 
-    <div class="w-full h-screen bg-[#171717]">
-
+    <div class="w-full h-screen flex justify-center items-center">
+        <div class="grid grid-cols-1 gap-4 h-96 w-80 shadow-xl place-items-center">
+            <div class="image-wrapper">
+                <img src="./src/img/product/seamers/Angelus Legacy Series Can Seamer- Model 10P.0.jpg" alt="">
+            </div>
+            <div class="text-center p-4">
+                <h2 class="text-lg font-bold">Angelus Legacy Series Can Seamer</h2>
+                <p class="text-gray-600">Model 10P</p>
+            </div>
+        </div>
     </div>
+
+    <div class="w-full h-screen flex justify-center items-center">
+        <div class=" image-container grid grid-cols-1 gap-4 h-96 w-80 shadow-xl place-items-center ">
+            <img src="./src/img/product/seamers/Angelus Legacy Series Can Seamer- Model 10P.jpg" alt=""
+                class="image1" />
+            <img src="./src/img/product/seamers/Angelus Legacy Series Can Seamer- Model 10P.0.jpg" alt=""
+                class="image2" />
+        </div>
+    </div>
+
+
 </body>
 
 </html>
